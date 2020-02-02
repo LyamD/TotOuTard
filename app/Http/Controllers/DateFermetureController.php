843d85969afb3461a567f5ac27303edb297df46d
LyamD <lyam_durand@outlook.fr>
2020-02-02 03:34:35 +0100

    ajout dateFermeture mc

---
+++ app/Http/Controllers/DateFermetureController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\dateFermeture;
+use Illuminate\Http\Request;
+
+class DateFermetureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\dateFermeture  $dateFermeture
+     * @return \Illuminate\Http\Response
+     */
+    public function show(dateFermeture $dateFermeture)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\dateFermeture  $dateFermeture
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(dateFermeture $dateFermeture)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\dateFermeture  $dateFermeture
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, dateFermeture $dateFermeture)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\dateFermeture  $dateFermeture
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(dateFermeture $dateFermeture)
+    {
+        //
+    }
+}
